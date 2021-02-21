8eef210c2a0ccd0bcb5a5c020f2397342e450593
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-02-21 11:01:20 -0600

    Integrated db connection

--- ems/app.js
+++ ems/app.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 const { response } = require('express');
 
+// Model
+var Employee = require('./models/employee')
+
+// Connect
+var mongoDB = 'mongodb+srv://ghend:Ub6iwve7@buwebdev-cluster-1.qqxjy.mongodb.net/test?authSource=admin&replicaSet=atlas-njj3c5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+db.once('open', function() {
+  console.log('Application connected to mLab MongoDB instance');
+});
+
+
+//Application
 var app = express();
 
 app.set('views', path.resolve(__dirname, 'views'));
